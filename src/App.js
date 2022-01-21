c7c20363fb4f3b12e18a0dd6d0e03deb56137a45
AnaM1005 <72257415+AnaM1005@users.noreply.github.com>
2022-01-21 01:11:31 +0000

    Initial deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello there! It's the new application.
         </a>
       </header>
     </div>
